aeaac4fbc83c018726bf15b8da75af5fd7e21dfa
Chet Murthy <chetsky@gmail.com>
2020-04-04 19:31:06 -0700

    meh

--- testsuite/grammar_bug_test.ml
+++ testsuite/grammar_bug_test.ml
@@ -27,17 +27,12 @@ Pcaml.inter_phrases.val := Some (";;\n") ;
 
 value has_argle = ref False ;
 
-value expr s = s |> Stream.of_string |> Grammar.Entry.parse Alt_pa_o.expr ;
 value sig_item s = s |> Stream.of_string |> Grammar.Entry.parse Alt_pa_o.sig_item ;
-value match_case s = s |> Stream.of_string |> Grammar.Entry.parse Alt_pa_o.match_case ;
 value argle1 s = s |> Stream.of_string |> Grammar.Entry.parse Alt_pa_o.argle1 ;
 value argle2 s = s |> Stream.of_string |> Grammar.Entry.parse Alt_pa_o.argle2 ;
 
 value tests () = "grammar_bug" >::: [
-    "simple" >:: (fun  [ _ ->
-      assert_equal ~{cmp=Reloc.eq_expr} <:expr< 1 >> (expr "1")
-    ])
-    ; "sig_item-open1" >:: (fun  [ _ ->
+    "sig_item-open1" >:: (fun  [ _ ->
       if has_argle.val then
       assert_raises_exn_pred (smart_exn_eq (Ploc.Exc Ploc.dummy 
                                               (Stdlib.Stream.Error "[ext_attributes] expected after 'open' (in [sig_item])")))
@@ -53,16 +48,6 @@ value tests () = "grammar_bug" >::: [
       else
         assert_equal ~{cmp=Reloc.eq_sig_item} <:sig_item< open A.B >> (sig_item "open A.B")
     ])
-    ; "match-case-1" >:: (fun  [ _ ->
-      ignore(match_case "1 -> 2")
-    ])
-    ; "argle1-1" >:: (fun [ _ ->
-      if has_argle.val then
-        ignore(argle1 "A")
-      else
-        assert_raises_exn_pred (smart_exn_eq (Ploc.Exc Ploc.dummy (Stdlib.Stream.Error "entry [argle1] is empty")))
-          (fun () -> ignore(argle1 "A"))
-                          ])
     ; "argle1-2" >:: (fun [ _ ->
       if has_argle.val then
         assert_raises_exn_pred (smart_exn_eq (Ploc.Exc Ploc.dummy (Stdlib.Stream.Error "illegal begin of argle1")))
